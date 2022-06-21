cc2166daa5273b17d78abb5e63e1a07e0b50f6ed
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2022-06-21 20:43:45 +0530

    Update Datasets.py
    datasets for calculations in hours are done

--- Datasets.py
+++ Datasets.py
@@ -1,7 +1,35 @@
 import random
+import datetime
 
-for i in range(1,101):
-    job='job '+str(i)
-    frequency=random.randint(1,10)
-    execution_time=random.randint(1,10)
+def datasets_for_hours(datasets_needed):                                                                 #this is for 24hrs(i.e. tasks for one single day)
+    i = 1
+    while (i<datasets_needed+1):
+        job='job '+str(i)
+        frequency = random.randint(1,10)
+        start_time = random.randint(1,23)
+        end_time = random.randint(2,24)
+        execution_time = random.randint(1,6)
+        desired_start_time = random.randint(2,21)
+        value = random.randint(1,100)
+        capacity = random.randint(1,80)
+        s_no = i
+        
+        if (start_time + execution_time > end_time):   i = i-1
+        elif (start_time>desired_start_time):   i = i-1
+        elif (desired_start_time+2+execution_time>end_time):  i = i-1
+        else:
+          print("s.no:-"+str(s_no))
+          print("job:-"+job)
+          print("start_time:-"+str(start_time))
+          print("end_time:-"+str(end_time))
+          print("execution_time:-"+str(execution_time))
+          print("desired_start_time:-"+str(desired_start_time))
+          print("frequency:-"+str(frequency))
+          print("value:-"+str(value))
+          print("capacity:-"+str(capacity))
+        i = i+1
+    return
     
+#main program
+datasets = int(input("enter the number of datasets needed:"))
+datasets_for_hours(datasets)
