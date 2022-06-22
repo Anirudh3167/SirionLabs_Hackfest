55a91b61ed3d25d03b02e6c786fc89f2264f295e
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2022-06-22 10:47:42 +0530

    Update Datasets.py
    Edit-3 now the hours accuracy is increased to minutes

--- Datasets.py
+++ Datasets.py
@@ -1,15 +1,22 @@
 import random
 import datetime
 
+def minutes(hours):
+    return hours*60
+
+def hours(minutes):
+    return minutes*60
+
 def datasets_for_hours(datasets_needed):                                                                 #this is for 24hrs(i.e. tasks for one single day)
     i = 1
+    print(minutes)
     while (i<datasets_needed+1):
         job='job '+str(i)
         frequency = random.randint(1,10)
-        start_time = random.randint(1,23)
-        end_time = random.randint(2,24)
-        execution_time = random.randint(1,6)
-        desired_start_time = random.randint(2,21)
+        start_time = random.randint(0,minutes(23))
+        end_time = random.randint(120,minutes(24))
+        execution_time = random.randint(60,360)
+        desired_start_time = random.randint(120,minutes(21))
         value = random.randint(1,100)
         capacity = random.randint(1,80)
         s_no = i
